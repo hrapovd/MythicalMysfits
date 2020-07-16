fffd66dd72a5e1a24d060f9edcfe8518d0844732
hrapovd <hrapovd@gmail.com>
2020-07-16 18:51:31 +0300

    terraform test build docker image.

---
+++ terr-test/app-img.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "null_resource" "test1" {
+  provisioner "local-exec" {
+    command = <<-EOF
+    docker build ../aws-modern-application-workshop/module-2/app/ \
+    -t ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/mythicalmysfits/service:latest
+    EOF
+  }
+}
+
